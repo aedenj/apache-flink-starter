4fc72e5d0a01a10b298da6f3040b349b1646abb7
Aeden Jameson <aeden.jameson@gmail.com>
2020-08-13 09:36:29 -0700

    fix broken test setup

---
+++ src/test/java/org/myorg/quickstart/SkeletonTest.java
@@ -0,0 +1,11 @@
+package org.myorg.quickstart.testing;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SkeletonTest {
+  @Test
+  public void TheAnswerToLife() {
+    assertThat(42).isEqualTo(Integer.sum(19,23));
+  }
+}
