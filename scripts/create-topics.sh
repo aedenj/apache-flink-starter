7be1633d069ac4cd9c138bdf8a5fc5d0bae8f811
Aeden Jameson <aeden.jameson@gmail.com>
2020-10-04 15:29:14 -0700

    update create topics script

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -3,5 +3,5 @@
 docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --delete --bootstrap-server broker-1:19092 --topic "source";
 docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --delete --bootstrap-server broker-1:19092 --topic destination;
 
-docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --create --bootstrap-server broker-1:19092 --partitions 9 --replication-factor 3 --topic "source";
-docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --create --bootstrap-server broker-1:19092 --partitions 9 --replication-factor 3 --topic destination;
+docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --create --bootstrap-server broker-1:19092 --partitions 9 --replication-factor 1 --topic "source";
+docker run --rm -i --network kafka-net wurstmeister/kafka:latest kafka-topics.sh --create --bootstrap-server broker-1:19092 --partitions 9 --replication-factor 1 --topic "destination";
