83bca82a6df9f8175114a435abfc274254ceb798
Aeden Jameson <aeden.jameson@gmail.com>
2020-10-04 22:27:14 -0700

    read config with lightbend config

--- src/main/java/org/myorg/quickstart/StreamingJob.java
+++ src/main/java/org/myorg/quickstart/StreamingJob.java
@@ -1,9 +1,8 @@
 /**
- * Empty package comment.
+ * Something profound.
  */
 package org.myorg.quickstart;
 
-import java.util.Properties;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -11,7 +10,7 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
 /**
- * Empty class comment.
+ * Starting ponit for a Flink streaming job using the DataStream API.
  */
 public final class StreamingJob {
     private StreamingJob() {
@@ -20,28 +19,24 @@ public final class StreamingJob {
     }
 
     public static void main(final String[] args) throws Exception {
+        final JobConfig config = new JobConfig();
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        final Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092, broker-1:19092");
-        properties.setProperty("group.id", "test");
 
         final FlinkKafkaConsumer<String> myConsumer = new FlinkKafkaConsumer<>(
             "source",
             new SimpleStringSchema(),
-            properties
+            config.kafka()
         );
 
-        final DataStream<String> msgs = env.addSource(myConsumer).name("Raw Messages Again");
+        final DataStream<String> msgs = env.addSource(myConsumer).name("Source Topic");
 
         final FlinkKafkaProducer<String> enriched = new FlinkKafkaProducer<>(
             "destination",
             new SimpleStringSchema(),
-            properties
+           config.kafka()
         );
 
-
-        msgs.addSink(enriched).name("Destination Topics");
+        msgs.addSink(enriched).name("Destination Topic");
         env.execute("Kafka Experiment");
     }
 }
