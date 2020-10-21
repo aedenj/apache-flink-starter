e493124b02f6fa85cf31d9ae35e1030bcafa7719
Aeden Jameson <aeden.jameson@gmail.com>
2020-10-20 22:23:19 -0700

    use the confluent image for tooling

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -2,6 +2,6 @@
 
 for var in "$@"
   do
-    docker exec -i kafka-tools kafka-topics.sh --delete --bootstrap-server broker-1:19092 --topic "$var";
-    docker exec -i kafka-tools kafka-topics.sh --create --bootstrap-server broker-1:19092 --partitions 1 --replication-factor 1 --topic "$var";
+    docker exec -i kafka-tools kafka-topics --delete --bootstrap-server broker-1:19092 --topic "$var";
+    docker exec -i kafka-tools kafka-topics --create --bootstrap-server broker-1:19092 --partitions 1 --replication-factor 1 --topic "$var";
   done
--- scripts/start-kafka-producer.sh
+++ scripts/start-kafka-producer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker exec -i kafka-tools kafka-console-producer.sh --broker-list broker-1:19092 --topic source --property "parse.key=true" --property "key.separator=:"
+docker exec -i kafka-tools kafka-console-producer --broker-list broker-1:19092 --topic source --property "parse.key=true" --property "key.separator=:"
